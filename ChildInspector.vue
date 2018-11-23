a07cd1d11c4895c537436ae7287d8133f18b7073
LorisC <loris@ciprin.fr>
2018-11-23 10:19:02 +0100

    first commit

---
+++ ChildInspector.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="child-inspector">
+        <div class="child-inspector-name">
+            <h1>
+                {{name}}
+            </h1>
+        </div>
+
+
+        <div class="child-inspector-child"
+             v-for="(info, index) in childInfo"
+             :key="index">
+            <div class="child-inspector-child-name">
+                <h2>
+                    {{info.name}}
+                </h2>
+            </div>
+
+
+            <spinal-icon-button
+                    class="child-inspector-delete-child"
+                    icon="delete"
+                    tool-tip="suppress child"
+            />
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    import NodeHeader from "./NodeHeader";
+    import SpinalIconButton from "../Button/SpinalIconButton";
+
+    export default {
+        name: "ChildInspector",
+        components: {SpinalIconButton, NodeHeader},
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            childInfo: {
+                type: Array,
+                default: function () {
+                    return [
+                        {name: "child 1"},
+                        {name: "child 2"},
+                        {name: "child 3"},
+                        {name: "child 4"}
+                    ];
+                }
+            },
+            defaultRelationName: {
+                type: String,
+                default: function () {
+                    return ""
+                }
+            }
+        },
+        data: function () {
+            return {
+                relationName: "hah"
+            }
+        },
+        mounted() {
+            this.relationName = this.defaultRelationName;
+        }
+
+    }
+</script>
+
+<style scoped>
+
+    .child-inspector-name {
+        height: 30px;
+    }
+
+    .child-inspector-child-name {
+        height: 55px;
+    }
+
+    .child-inspector-child {
+        border: 1px solid #979797;
+        position: relative;
+        width: 100%;
+        height: 55px;
+    }
+
+    .child-inspector-delete-child {
+        position: absolute;
+        right: 0;
+        top: 5px;
+    }
+
+
+</style>
---
+++ NodesList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="nodes-list">
+        <node-item class="nodes-list-context"
+                   v-for="(id, index) in contextIds"
+                   :key="index"
+                   :nodes="nodes"
+                   :node-id="id"
+                   @node-selected="$emit('node-selected', $event)"
+                   @hide-bim-object="$emit('hide-bim-object', $event)"
+                   @pull-children="$emit('pull-children',$event)"
+                   background-color="rgba(46, 46, 46, 0.5)"
+        />
+    </div>
+
+</template>
+
+<script>
+    import NodeItem from "./NodeItem.vue";
+
+    export default {
+        name: "NodesList",
+        components: {NodeItem},
+        props: {
+            nodes: {
+                type: Object,
+                default: function () {
+                    return []
+                }
+            },
+
+            contextIds: {
+                type: Array,
+                default: function () {
+                    return []
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .nodes-list {
+        width: 100%;
+    }
+
+    .nodes-list-context {
+
+    }
+</style>
